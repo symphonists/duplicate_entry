9f6070aa9979035ebe64501b9fdffea880904f10
Alexander Rutz <ar@animaux.de>
2014-08-13 17:25:46 +0200

    Add german translation

---
+++ lang/lang.de.php
@@ -0,0 +1,21 @@
+<?php
+
+  $about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Alexander Rutz',
+			'email' => 'alex@animaux.de',
+			'website' => 'http://animaux.de'
+		),
+		'release-date' => '2014-08-13'
+	);
+
+	/**
+	 * Duplicate Entry
+	 */
+	$dictionary = array(
+	
+		'Save as New' => 
+		'Als neuen Eintrag speichern',
+
+	);
